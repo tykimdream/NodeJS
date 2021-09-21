a5037f36a40b869887852326f15ce02e7f871233
tykimdream@korea.ac.kr <tykimdream@korea.ac.kr>
2021-09-21 21:06:24 +0900

    render

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ Node JS 공부
 
 Web Development with Node & Express
 한 권으로 끝내는 Node & Express 2판
+
+node_modules: npm package
+public: img등
+views: layout
--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
-
+const fortune = require('./lib/fortuneCookies')
 const app = express()
 
 // configure Handlebars view engine
@@ -12,18 +12,11 @@ app.set('view engine', 'handlebars')
 app.use(express.static(__dirname + '/public'))
 
 const port = process.env.PORT || 3000
-const fortunes = [
-  "Conquer your fears or they will conquer you",
-  "Rivers need springs.",
-  "Do not fear what you don't know",
-  "You will have a pleasant surprise",
-  "Whenever possible, keep it simple.",
-]
+
 
 app.get('/', (req, res) => res.render('home'))
 app.get('/about', (req, res) => {
-  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-  res.render('about', {fortune: randomFortune})
+  res.render('about', {fortune: fortune.getFortune()})
 })
 
 // custom 404 page
